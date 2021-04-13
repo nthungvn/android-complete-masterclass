3ddca66d4b9d78c68c33bf9df4b75e3628c694da
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-13 17:15:33 +0700

    LinearLayout

--- app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
@@ -14,14 +14,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void buttonClicked(View view) {
-        TextView output = findViewById(R.id.bmiResult);
-        TextView height = findViewById(R.id.yourHeight);
-        TextView weight = findViewById(R.id.yourWeight);
-        Float heightValue = Float.valueOf(height.getText().toString());
-        Float weightValue = Float.valueOf(weight.getText().toString());
-        Float bmiResult = heightValue / (weightValue * weightValue);
-        output.setText(bmiResult.toString());
-    }
 }
