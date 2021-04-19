3da5b742636d755677e9233c786c8652732b5669
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-19 21:36:42 +0700

    show the detail of food

---
+++ app/src/main/java/vn/vl/nthung/androidcm/FoodActivity.java
@@ -0,0 +1,26 @@
+package vn.vl.nthung.androidcm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FoodActivity extends AppCompatActivity {
+
+    public static final String EXTRAS_FOOD_NO = "FOOD_NO";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food);
+
+        int foodNo = getIntent().getIntExtra(EXTRAS_FOOD_NO, 0);
+        Food food = Food.foods[foodNo];
+
+        TextView name = (TextView) findViewById(R.id.food_name);
+        name.setText(food.getName());
+
+        TextView description = (TextView) findViewById(R.id.food_description);
+        description.setText(food.getDescription());
+    }
+}
--- app/src/main/java/vn/vl/nthung/androidcm/FoodCategoryActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/FoodCategoryActivity.java
@@ -1,12 +1,12 @@
 package vn.vl.nthung.androidcm;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.ListViewCompat;
-
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class FoodCategoryActivity extends ListActivity {
 
@@ -15,5 +15,13 @@ public class FoodCategoryActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         ListAdapter adapter = new ArrayAdapter<Food>(this, android.R.layout.simple_list_item_1, Food.foods);
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView listview, View view, int position, long id) {
+        super.onListItemClick(listview, view, position, id);
+        Intent intent = new Intent(this, FoodActivity.class);
+        intent.putExtra(FoodActivity.EXTRAS_FOOD_NO, id);
+        startActivity(intent);
     }
 }
