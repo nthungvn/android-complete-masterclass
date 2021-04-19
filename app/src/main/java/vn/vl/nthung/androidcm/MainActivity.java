091e08ff210ae0439b20dcb38d232f3b5cd45c4f
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-19 10:31:34 +0700

    Add event listener in java code

--- app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
@@ -5,24 +5,22 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate");
-    }
-
-    public void onSwitchButtonClicked(View e) {
-        Intent intent = new Intent(this, ActivityTwo.class);
-        startActivity(intent);
+        Button switchButton = (Button) findViewById(R.id.switchButton);
+        switchButton.setOnClickListener((View.OnClickListener) v -> {
+            TextView textView = (TextView) findViewById(R.id.greeting);
+            textView.setText("Button clicked");
+        });
     }
 }
