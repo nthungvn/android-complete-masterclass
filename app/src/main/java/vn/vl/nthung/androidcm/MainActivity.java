894040ceba0be7d1c55ed079a38891dcb74ac56b
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-19 10:09:12 +0700

    switch activity by intent

--- app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
@@ -1,17 +1,28 @@
 package vn.vl.nthung.androidcm;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+    }
+
+    public void onSwitchButtonClicked(View e) {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
     }
 }
