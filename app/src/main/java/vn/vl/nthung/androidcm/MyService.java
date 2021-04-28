f08497d9c894f08a3f92ed7d7cc37a8f29c4154f
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-28 16:24:23 +0700

    show notification when service started

--- app/src/main/java/vn/vl/nthung/androidcm/MyService.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MyService.java
@@ -1,7 +1,10 @@
 package vn.vl.nthung.androidcm;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
@@ -11,12 +14,13 @@ import android.os.Process;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-
-import com.google.android.material.snackbar.Snackbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MyService extends Service {
     private Looper serviceLooper;
     private ServiceHandler serviceHandler;
+    private static final String CHANNEL_ID = "123";
 
     @Override
     public void onCreate() {
@@ -24,6 +28,7 @@ public class MyService extends Service {
         thread.start();
         serviceLooper = thread.getLooper();
         serviceHandler = new ServiceHandler(serviceLooper);
+        createNotificationChannel();
     }
 
     @Override
@@ -33,6 +38,15 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.outline_home_black_24dp)
+                .setContentTitle("Service started")
+                .setContentText("Your service is running in the background and will be stopped in 5 seconds")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("Your service is running in the background and will be stopped in 5 seconds"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(123, builder.build());
         Toast.makeText(this, "Service started", Toast.LENGTH_LONG).show();
         Message message = serviceHandler.obtainMessage();
         message.arg1 = startId;
@@ -43,6 +57,19 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         Toast.makeText(this, "Service done", Toast.LENGTH_LONG).show();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notify Center";
+            String description = "Notify Center in app";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     class ServiceHandler extends Handler {
