5dc8e9cf823afc128f3db6133f94c315d1d8d388
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-30 19:19:58 +0700

    play audio

--- app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
@@ -2,7 +2,9 @@ package vn.vl.nthung.androidcm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener((view) -> {
+            MediaPlayer player = MediaPlayer.create(this, R.raw.tone_sms);
+            player.start();
+        });
     }
 }
