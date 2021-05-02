7753311db8333f68ac07ede0faf8274ace9d5c2d
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-02 09:29:57 +0700

    save image to file

--- app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
@@ -1,23 +1,31 @@
 package vn.vl.nthung.androidcm;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.io.FileDescriptor;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PICK_FILE_REQUEST = 1;
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView topMemeText;
     private TextView bottomMemeText;
 
+    private ConstraintLayout memeResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
         bottomText = findViewById(R.id.bottom_text);
         topMemeText = findViewById(R.id.top_meme_text);
         bottomMemeText = findViewById(R.id.bottom_meme_text);
+
+        memeResult = findViewById(R.id.meme_result);
 
         selectImageBtn.setOnClickListener(onSelectImageClickedHandler);
         previewBtn.setOnClickListener(onPreviewClickedHandler);
@@ -91,5 +103,26 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private View.OnClickListener onSaveClickedHandler = (view) -> {
+        memeResult.setDrawingCacheEnabled(true);
+        memeResult.buildDrawingCache();
+        Bitmap screenshot = Bitmap.createBitmap(memeResult.getDrawingCache());
+
+        String imageName = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date()) + ".jpg";
+        try {
+            OutputStream outputStream = null;
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.DISPLAY_NAME, imageName);
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_DCIM + "/Screenshots");
+            Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            outputStream = getContentResolver().openOutputStream(uri, "rw");
+            screenshot.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.close();
+            memeResult.setDrawingCacheEnabled(false);
+            Toast.makeText(this, "Saved image", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     };
 }
