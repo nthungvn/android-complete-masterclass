16dbdff0dcde7d9c9e65e1ce7192b12674d3f24d
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-02 16:08:28 +0700

    update data to server

--- app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
@@ -1,21 +1,34 @@
 package vn.vl.nthung.androidcm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("message");
-        ref.setValue("Hello world");
+        Button updateBtn = findViewById(R.id.update_btn);
+        updateBtn.setOnClickListener(onUpdateClicked);
     }
+
+    private View.OnClickListener onUpdateClicked = view -> {
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("message");
+        EditText message = findViewById(R.id.message);
+        ref.setValue(message.getText().toString());
+        Toast.makeText(this, "Data is updated", Toast.LENGTH_LONG).show();
+    };
 }
