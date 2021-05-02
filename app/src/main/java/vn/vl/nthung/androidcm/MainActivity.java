df26ecd79e97c0252bf1e51d50cc42e894f91174
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-02 16:33:07 +0700

    real time update data from server

--- app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
+++ app/src/main/java/vn/vl/nthung/androidcm/MainActivity.java
@@ -6,29 +6,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
     private FirebaseDatabase database;
+    private EditText message;
+    private DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        message = findViewById(R.id.message);
         Button updateBtn = findViewById(R.id.update_btn);
         updateBtn.setOnClickListener(onUpdateClicked);
+
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("message");
+        ref.addValueEventListener(new OnDataChangeHandler());
     }
 
     private View.OnClickListener onUpdateClicked = view -> {
-        database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("message");
-        EditText message = findViewById(R.id.message);
         ref.setValue(message.getText().toString());
         Toast.makeText(this, "Data is updated", Toast.LENGTH_LONG).show();
     };
+
+    class OnDataChangeHandler implements ValueEventListener {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            message.setText(snapshot.getValue(String.class));
+            Toast.makeText(MainActivity.this, "Server data has been changed", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    }
 }
